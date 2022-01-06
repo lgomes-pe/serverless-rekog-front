4ff50146d7b546c5852d8c6f071c4f492e3d166a
lgomes-pe <lgpboss@hotmail.fr>
2022-01-06 10:40:04 +0100

    test

--- src/backup/App.js
+++ src/backup/App.js
@@ -1,37 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "react-dropzone-uploader/dist/styles.css";
-import Dropzone, { defaultClassNames } from "react-dropzone-uploader";
+import Dropzone from "react-dropzone-uploader";
 import axios from "axios";
 
-//Uploader layout
-const Layout = ({input, previews, submitButton, dropzoneProps, files, extra: {maxFiles}}) => {
-  return (
-    <div id="div1">
-      <div id="uploader">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Uploader
-        </p>
-        <div {...dropzoneProps}>
-          {files.length < maxFiles && input}
-        </div>
-
-        {files.length > 0 && submitButton}
-      </div>
-
-      <div id="preview">
-        {previews}
-      </div>
-    </div>
-  )
-}
 
 const Uploader = () => {
   const axios = require("axios").default;
 
-  const API_ENDPOINT = 'https://onhw31kegd.execute-api.eu-central-1.amazonaws.com/default/getSignedUrl'
+  const API_ENDPOINT = 'https://gpzvvcu9c8.execute-api.us-east-2.amazonaws.com/default/getPresignedURL-test'
   const handleChangeStatus = ({meta, remove}, status) => {
     console.log(status, meta);
   };
@@ -53,24 +31,18 @@ const Uploader = () => {
     }
   };
 
-
   return (
     <Dropzone
       onChangeStatus = {handleChangeStatus}
       onSubmit = {handleSubmit}
-      maxFiles = {10}
+      maxFiles = {100}
       multiple = {true}
       canCancel = {true}
-      LayoutComponent={Layout}
-      classNames={{inputLabelWithFiles: defaultClassNames.inputLabel}}
-      inputContent = {(files, extra) => (extra.reject ? 'Only images are allowed !' : "Drop Files or Click to browse")}
+      inputContent = "Drop Files"
       styles = {{
         dropzone: {width: 400, height: 200},
         dropzoneActive: {borderColor: "blue"},
-        dropzoneReject: {borderColor: 'FF0000', backgroundColor: '#F1BDAB'},
-        inputLabel: (files, extra) => (extra.reject ? {color: '#A02800'} : {})
       }}
-      accept = "image/*"
     />
   );
 }
@@ -82,7 +54,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Uploader
+        </p>
         <Uploader />
       </header>
     </div>
